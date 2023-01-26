1ec31203ccc5f6761ef26a8539b1f4ef2dbdf0b4
subramaneshwar_8 <42493880+subramaneshwar@users.noreply.github.com>
2023-01-26 16:07:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const input = document.getElementById("input")
 async function myfunction(){
     let search = input.value;
     console.log(search)
+    bottom.innerHTML=""
     const resp = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
     const data = await resp.json()
     console.log(data.meals)
